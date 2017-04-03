ee83dc46bc0b0ac03efa61293f7033ed59d1f689
CELIA <c.martinher@outlook.es>
2017-04-03 18:23:56 +0200

    ejercicio resuelto

---
+++ flickr.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    function search(search){
+        $.getJSON("http://api.flickr.com/services/feeds/photos_public.gne?tags=" + search + "&tagmode=any&format=json&jsoncallback=?")
+        .done(function(data) {
+            var i = 0;
+            $(data.items).each(function (){
+                var fotos = data.items[i].media.m
+                $("#fotos").append("<img src = " + fotos + ">")
+                i++;
+            });
+        });
+    }
+    $("#flickr").click(function(){
+        search("fuenlabrada");
+    });
+});
